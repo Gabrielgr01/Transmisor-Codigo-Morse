e0f655d2ddaba5309883717bedc0454038915ede
Jose Fabio Navarro Naranjo <josefabio1127@gmail.com>
2022-01-21 11:57:26 -0600

    Actualización del comparador
    Se añadió una variable más de entrada para poder controlar el valor a comparar desde el exterior. Se simplificó el código.

--- Verilog/sources/comparador_frec.v
+++ Verilog/sources/comparador_frec.v
@@ -21,13 +21,14 @@
 
 
 module comparador_frec(
-    input[27:0] Q,
-    output reg CLK_FF10Hz
+    input[27:0] entrada1,
+    input[27:0] entrada2,
+    output reg salida
     );
     
-    always @(Q)
-    begin
-        if (Q == 28'd99999999) CLK_FF10Hz <= 1'd1;
-        else CLK_FF10Hz <= 1'd0;
-    end
+    always @(entrada2)
+        if (entrada1 == entrada2) 
+            salida <= 1'b1;
+        else 
+            salida <= 1'b0;
 endmodule
